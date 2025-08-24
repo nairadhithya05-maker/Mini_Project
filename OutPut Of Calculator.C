7566cb415b4b39b88ac2d331f10c5b1e86b0d6b4
Nair Adhithya <nairadhithya05@gmail.com>
2025-08-24 20:05:32 +0530

    Update OutPut Of Calculator.C

--- OutPut Of Calculator.C
+++ OutPut Of Calculator.C
@@ -1,1 +1,17 @@
+Example 1: Addition
+Enter an operator (+, -, *, /): +
+Enter two numbers: 53
+5.00 +3.00 = 8.00
 
+Example 2: Division
+Enter an operator (+, -, *, /): /Enter two numbers: 10 4
+10.00/4.00 2.50
+
+Example 3: Division by Zero (Error Handling)
+Enter an operator (+, -, *, /): /Error! Division by zero is not allowed.
+Enter two numbers: 80
+
+Example 4: Invalid Operator
+Enter an operator (+, -, *, /): &
+Enter two numbers: 10 5
+Error! Invalid operator.
